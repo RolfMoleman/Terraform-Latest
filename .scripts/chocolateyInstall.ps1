c664bf669cc18dbc5f6d2df517a1ec3a7f0753c3
Carl Dawson <106377191+RolfMoleman@users.noreply.github.com>
2023-12-14 16:21:37 +0000

    INTIAL WORK

---
+++ .scripts/chocolateyInstall.ps1
@@ -0,0 +1,38 @@
+﻿$ErrorActionPreference = 'Stop'
+
+# DO NOT CHANGE THESE MANUALLY. USE update.ps1
+$url = 'https://releases.hashicorp.com/terraform/1.6.5/terraform_1.6.5_windows_386.zip'
+$url64 = 'https://releases.hashicorp.com/terraform/1.6.5/terraform_1.6.5_windows_amd64.zip'
+$checksum = 'f19e749bff7c5a44c07aa8ec7efe8e3986105691b92df93f62de22efb523a983'
+$checksum64 = '5c4644f8c83f3851b70e3c3a52c68e36949b1f8436617577a69af162b4a3c815'
+
+$unzipLocation = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+if ([System.IO.Directory]::Exists("$env:ChocolateyInstall\lib\terraform")) {
+  if ([System.IO.Directory]::Exists("$env:ChocolateyInstall\lib\terraform\tools")) {
+    # clean old plugins and ignore files
+    Write-Host "Removing old terraform plugins"
+    Remove-Item "$env:ChocolateyInstall\lib\terraform\tools\terraform-*.*"
+  }
+}
+else {
+  if ([System.IO.Directory]::Exists("$env:ALLUSERSPROFILE\chocolatey\lib\terraform")) {
+    if ([System.IO.Directory]::Exists("$env:ALLUSERSPROFILE\chocolatey\lib\terraform\tools")) {
+      # clean old plugins and ignore files
+      Write-Host "Removing old terraform plugins"
+      Remove-Item "$env:ALLUSERSPROFILE\chocolatey\lib\terraform\tools" -Include "terraform-*.*"
+    }
+  }
+}
+
+$packageParams = @{
+  PackageName   = "terraform"
+  UnzipLocation = $unzipLocation
+  Url           = $url
+  Url64         = $url64
+  Checksum      = $checksum
+  Checksum64    = $checksum64
+  ChecksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageParams

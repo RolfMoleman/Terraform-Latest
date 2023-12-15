86ca9a8c4ea06255123ee1facbfff778a6294458
Carl Dawson <106377191+RolfMoleman@users.noreply.github.com>
2023-12-15 12:38:37 +0000

    documentaion and config tweaks

--- .scripts/update_nuspec.ps1
+++ .scripts/update_nuspec.ps1
@@ -1,24 +1,22 @@
-#Install-Module -Name AU
-
 #Requires -Version 5.0
 #Requires -Modules AU
 [cmdletbinding()]
 param (
-  [switch]$Force
+    [switch]$Force
 )
 
 Set-StrictMode -Version Latest
 $ProgressPreference = 'SilentlyContinue'
 $ErrorActionPreference = 'Stop'
 trap {
-    Write-Host "ERROR: $_"
-    ($_.ScriptStackTrace -split '\r?\n') -replace '^(.*)$','ERROR: $1' | Write-Host
-    ($_.Exception.ToString() -split '\r?\n') -replace '^(.*)$','ERROR EXCEPTION: $1' | Write-Host
+    Write-Output "ERROR: $_"
+    ($_.ScriptStackTrace -split '\r?\n') -replace '^(.*)$', 'ERROR: $1' | Write-Output
+    ($_.Exception.ToString() -split '\r?\n') -replace '^(.*)$', 'ERROR EXCEPTION: $1' | Write-Output
     throw
 }
 
 # See https://checkpoint.hashicorp.com/ and https://www.hashicorp.com/blog/hashicorp-fastly/#json-api
-$checkpointUrl = "https://checkpoint-api.hashicorp.com/v1/check/terraform"
+$checkpointUrl = 'https://checkpoint-api.hashicorp.com/v1/check/terraform'
 
 $releaseNotesTemplate = @'
 
@@ -26,93 +24,94 @@ $releaseNotesTemplate = @'
 For more information on previous releases, check out the changelog on [GitHub]({1}).
 '@
 
-function Get-ReleaseNotes($version, $changelogUrl) {
-  $rawChangelogUrl = "https://raw.githubusercontent.com/hashicorp/terraform/v$($version)/CHANGELOG.md"
-  $fullChangelog = Invoke-RestMethod -Uri $rawChangelogUrl
+function Get-ReleaseNote($version, $changelogUrl) {
+    $rawChangelogUrl = "https://raw.githubusercontent.com/hashicorp/terraform/v$($version)/CHANGELOG.md"
+    $fullChangelog = Invoke-RestMethod -Uri $rawChangelogUrl
 
-  # get everyting from the first "##" up until the second "##"
-  # note: [\s\S]* is used to select all characters including newline characters
-  $changelogMatches = Select-String -InputObject $fullChangelog -Pattern '\A(##[\s\S]*?)##[\s\S]*\z' -AllMatches
+    # get everything from the first "##" up until the second "##"
+    # note: [\s\S]* is used to select all characters including newline characters
+    $changelogMatches = Select-String -InputObject $fullChangelog -Pattern '\A(##[\s\S]*?)##[\s\S]*\z' -AllMatches
 
-  $latestChanges = $changelogMatches.Matches.Groups[1].Value
-  if (-not $latestChanges) {
-    throw "Could not get latest changes from $rawChangelogUrl"
-  }
+    $latestChanges = $changelogMatches.Matches.Groups[1].Value
+    if (-not $latestChanges) {
+        throw "Could not get latest changes from $rawChangelogUrl"
+    }
 
-  return $releaseNotesTemplate -f $latestChanges, $changelogUrl
+    return $releaseNotesTemplate -f $latestChanges, $changelogUrl
 }
 
-function Set-ReleaseNotes($nuspec, $releaseNotes) {
-  [xml]$xml = Get-Content $nuspec
-  $xml.package.metadata.releaseNotes = $releaseNotes
-  $xml.Save($nuspec)
+function Set-ReleaseNote($nuspec, $releaseNotes) {
+    [xml]$xml = Get-Content $nuspec
+    $xml.package.metadata.releaseNotes = $releaseNotes
+    $xml.Save($nuspec)
 }
 
 function global:au_AfterUpdate {
-  Write-Verbose (ConvertTo-Json $Latest)
-  # Get the latest changes and set as <releaseNotes /> in our nuspec
-  # Note: Cannot use au_SearchReplace for the releaseNotes because they are multi-lined
-  $releaseNotes = Get-ReleaseNotes $Latest.Version $Latest.ChangelogUrl
-  Write-Verbose $releaseNotes
-  $nuspec = Join-Path $PSScriptRoot "$($Latest.PackageName).nuspec" -Resolve
-  Set-ReleaseNotes $nuspec $releaseNotes
+    Write-Verbose (ConvertTo-Json $Latest)
+    # Get the latest changes and set as <releaseNotes /> in our nuspec
+    # Note: Cannot use au_SearchReplace for the releaseNotes because they are multi-lined
+    $releaseNotes = Get-ReleaseNotes $Latest.Version $Latest.ChangelogUrl
+    Write-Verbose $releaseNotes
+    $packagespath = '../../.packages'
+    $nuspec = Join-Path $packagespath "$($Latest.PackageName).nuspec" -Resolve
+    Set-ReleaseNotes $nuspec $releaseNotes
 }
 
 function global:au_SearchReplace {
-  @{
-    ".\chocolateyInstall.ps1" = @{
-      "(?i)(^[$]url\s*=\s*)'.*'"        = "`${1}'$($Latest.URL32)'"
-      "(?i)(^[$]url64\s*=\s*)'.*'"      = "`${1}'$($Latest.URL64)'"
-      "(?i)(^[$]checksum\s*=\s*)'.*'"   = "`${1}'$($Latest.Checksum32)'"
-      "(?i)(^[$]checksum64\s*=\s*)'.*'" = "`${1}'$($Latest.Checksum64)'"
+    @{
+        '.\chocolateyInstall.ps1' = @{
+            "(?i)(^[$]url\s*=\s*)'.*'"        = "`${1}'$($Latest.URL32)'"
+            "(?i)(^[$]url64\s*=\s*)'.*'"      = "`${1}'$($Latest.URL64)'"
+            "(?i)(^[$]checksum\s*=\s*)'.*'"   = "`${1}'$($Latest.Checksum32)'"
+            "(?i)(^[$]checksum64\s*=\s*)'.*'" = "`${1}'$($Latest.Checksum64)'"
+        }
     }
-  }
 }
 
-function Get-Shasums($url) {
-  $response = Invoke-RestMethod -Uri $url
-  Write-Verbose $response
-  $shasums = @{}
-  foreach ($line in ($response -split '\n')) {
-    if ($line) {
-      $sha, $file = $line -split '  '
-      $shasums[$file] = $sha
+function Get-Shasum($url) {
+    $response = Invoke-RestMethod -Uri $url
+    Write-Verbose $response
+    $shasums = @{}
+    foreach ($line in ($response -split '\n')) {
+        if ($line) {
+            $sha, $file = $line -split '  '
+            $shasums[$file] = $sha
+        }
     }
-  }
-  return $shasums
+    return $shasums
 }
 
 function Get-TerraformBuild($builds, $os, $arch) {
-  foreach ($build in $builds) {
-    if (($build.os -eq $os) -and ($build.arch -eq $arch)) {
-      return $build
+    foreach ($build in $builds) {
+        if (($build.os -eq $os) -and ($build.arch -eq $arch)) {
+            return $build
+        }
     }
-  }
-  throw "Terraform build for os = $os and arch = $arch not found"
+    throw "Terraform build for os = $os and arch = $arch not found"
 }
 
 function global:au_GetLatest {
-  $terraformCheckpoint = Invoke-RestMethod -Uri $checkpointUrl
-  Write-Verbose (ConvertTo-Json $terraformCheckpoint)
-  $currentDownloadUrl = $terraformCheckpoint.current_download_url
-  $terraformBuilds = Invoke-RestMethod -Uri "$($currentDownloadUrl)/index.json"
-  Write-Verbose (ConvertTo-Json $terraformBuilds)
-  $shasums = Get-Shasums "$($currentDownloadUrl)/$($terraformBuilds.shasums)"
-  Write-Verbose (ConvertTo-Json $shasums)
+    $terraformCheckpoint = Invoke-RestMethod -Uri $checkpointUrl
+    Write-Verbose (ConvertTo-Json $terraformCheckpoint)
+    $currentDownloadUrl = $terraformCheckpoint.current_download_url
+    $terraformBuilds = Invoke-RestMethod -Uri "$($currentDownloadUrl)/index.json"
+    Write-Verbose (ConvertTo-Json $terraformBuilds)
+    $shasums = Get-Shasums "$($currentDownloadUrl)/$($terraformBuilds.shasums)"
+    Write-Verbose (ConvertTo-Json $shasums)
 
-  $build32 = Get-TerraformBuild $terraformBuilds.builds 'windows' '386'
-  Write-Verbose (ConvertTo-Json $build32)
-  $build64 = Get-TerraformBuild $terraformBuilds.builds 'windows' 'amd64'
-  Write-Verbose (ConvertTo-Json $build64)
+    $build32 = Get-TerraformBuild $terraformBuilds.builds 'windows' '386'
+    Write-Verbose (ConvertTo-Json $build32)
+    $build64 = Get-TerraformBuild $terraformBuilds.builds 'windows' 'amd64'
+    Write-Verbose (ConvertTo-Json $build64)
 
-  return @{
-    Version      = $terraformCheckpoint.current_version
-    URL32        = $build32.url
-    URL64        = $build64.url
-    Checksum32   = $shasums[$build32.filename]
-    Checksum64   = $shasums[$build64.filename]
-    ChangelogUrl = $terraformCheckpoint.current_changelog_url
-  }
+    return @{
+        Version      = $terraformCheckpoint.current_version
+        URL32        = $build32.url
+        URL64        = $build64.url
+        Checksum32   = $shasums[$build32.filename]
+        Checksum64   = $shasums[$build64.filename]
+        ChangelogUrl = $terraformCheckpoint.current_changelog_url
+    }
 }
 
 # TLS 1.2 required by terraform's apis
--- versions.tf
+++ versions.tf
@@ -1,3 +1,5 @@
+# VERSION FILE USED FOR DEPENDABOT AND RENOVATE ONLY
+
 terraform {
   required_version = "1.6.6"
 }
